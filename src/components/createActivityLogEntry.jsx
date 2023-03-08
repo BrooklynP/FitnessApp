d21e200916a04fa2ae43e1f5f024208bc2b82c66
BrooklynP <Brooklynpedley@gmail.com>
2023-03-08 13:27:25 +0000

    Improved form

--- src/components/createActivityLogEntry.jsx
+++ src/components/createActivityLogEntry.jsx
@@ -7,14 +7,32 @@ class CreateActivityLogEntry extends React.Component {
     }
 
     logEntryButtonClicked(){
-      const logDesc = document.getElementById("logEntryInput").value
-      this.props.logEntry(logDesc)
+      const exerciseType = document.getElementById("logEntryExerciseInput").value
+      const reps = document.getElementById("logEntryRepsInput").value
+      const weight = document.getElementById("logEntryWeightInput").value
+      const description = document.getElementById("logEntryDescInput").value
+      this.props.logEntry(exerciseType + " x " + reps + " x " + weight + "kg")
     }
 
    render() {
     return (
       <div>
-        <input ID="logEntryInput"></input>
+        <label>Exercise</label><br></br>
+        {/* <select name="" id="exerciseDropdown">
+          <option value="0"></option>
+          <option value="1">Shoulder Press</option>
+          <option value="1">Bench Press</option>
+          <option value="1">Push Ups</option>
+          <option value="1">Dips</option>
+          <option value="1">Shrugs</option>
+        </select> */}
+        <input ID="logEntryExerciseInput"></input><br></br>
+        <label>Reps</label><br></br>
+        <input ID="logEntryRepsInput"></input><br></br>
+        <label>Weight</label><br></br>
+        <input ID="logEntryWeightInput"></input><br></br>
+        <label>Notes</label><br></br>
+        <input ID="logEntryDescInput"></input><br></br>
         <button className="primary" onClick={this.logEntryButtonClicked}>Log</button>
       </div>
     );
