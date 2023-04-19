8b4346bd959406ade32de5ee7b69e1e231a3208d
BrooklynP <Brooklynpedley@gmail.com>
2023-04-19 13:53:32 +0100

    Refactor to function with hooks

--- src/components/createActivityLogEntry.jsx
+++ src/components/createActivityLogEntry.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 
-class CreateActivityLogEntry extends React.Component {
-    constructor(props){
-        super(props);
-        this.logEntryButtonClicked = this.logEntryButtonClicked.bind(this);
-    }
+function CreateActivityLogEntry(props){
+  const [selectedExercise, setSelectedExercise] = React.useState("");
 
-    logEntryButtonClicked(){
-      const exerciseType = document.getElementById("logEntryExerciseInput").value
-      const reps = document.getElementById("logEntryRepsInput").value
-      const weight = document.getElementById("logEntryWeightInput").value
-      const description = document.getElementById("logEntryDescInput").value
-      this.props.logEntry(exerciseType + " x " + reps + " x " + weight + "kg")
-    }
-
-   render() {
-    return (
-      <div>
-        <label>Exercise</label><br></br>
-        {/* <select name="" id="exerciseDropdown">
-          <option value="0"></option>
-          <option value="1">Shoulder Press</option>
-          <option value="1">Bench Press</option>
-          <option value="1">Push Ups</option>
-          <option value="1">Dips</option>
-          <option value="1">Shrugs</option>
-        </select> */}
-        <input ID="logEntryExerciseInput"></input><br></br>
-        <label>Reps</label><br></br>
-        <input ID="logEntryRepsInput"></input><br></br>
-        <label>Weight</label><br></br>
-        <input ID="logEntryWeightInput"></input><br></br>
-        <label>Notes</label><br></br>
-        <input ID="logEntryDescInput"></input><br></br>
-        <button className="primary" onClick={this.logEntryButtonClicked}>Log</button>
-      </div>
-    );
+  const logEntryButtonClicked = () => {
+    const exerciseType = selectedExercise;
+    const reps = document.getElementById("logEntryRepsInput").value
+    const weight = document.getElementById("logEntryWeightInput").value
+    const description = document.getElementById("logEntryDescInput").value
+    props.logEntry(exerciseType + " x " + reps + " x " + weight + "kg")
   }
+
+  return (
+    <div>
+      <label>Exercise</label><br></br>
+      <select name="" id="exerciseDropdown" onChange={(e)=>{setSelectedExercise(e.target.value)}}>
+        <option value=""></option>
+        <option value="Shoulder Press">Shoulder Press</option>
+        <option value="Bench Press">Bench Press</option>
+        <option value="Push Ups">Push Ups</option>
+        <option value="Dips">Dips</option>
+        <option value="Shrugs">Shrugs</option>
+      </select><br></br>
+      <label>Reps</label><br></br>
+      <input ID="logEntryRepsInput"></input><br></br>
+      <label>Weight</label><br></br>
+      <input ID="logEntryWeightInput"></input><br></br>
+      <label>Notes</label><br></br>
+      <input ID="logEntryDescInput"></input><br></br>
+      <button className="primary" onClick={logEntryButtonClicked}>Log</button>
+    </div>
+  );
 }
 
 export default CreateActivityLogEntry;
