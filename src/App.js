a9edba7419480e504da313c97ef603cba5e4f1fb
BrooklynP <Brooklynpedley@gmail.com>
2023-06-19 14:55:59 +0100

    Create indexedDB on page load

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ import EntryForm from './pages/EntryForm';
 const appTitle = "Fitness App";
 
 function App() {
+  if (!('indexedDB' in window)) {
+    return <p>This browser doesn't support IndexedDB - A technology required for this app to work</p>;
+  }
+
+  const requestDBOpen = window.indexedDB.open("fitness-app", 1)
+  requestDBOpen.onerror = (event) => {
+    console.error("Why didn't you allow my web app to use IndexedDB?!", event);
+  };
+
+  requestDBOpen.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    if (!db.objectStoreNames.contains("workouts")) {
+      db.createObjectStore("workouts", { keyPath: "id", autoIncrement: true });
+    }
+  };
+
   return (
     <div className="App">
       <FitnessHeader appTitle={appTitle}/>
