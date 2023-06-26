dd16a6ed941f1f84b04c8563087f6638ce9acc97
BrooklynP <Brooklynpedley@gmail.com>
2023-06-26 18:29:22 +0100

    Creating nav on homage page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PageTitle from '../coreComponents/pageTitle';
 
 class Home extends React.Component {
-   render() {
+  render() {
     return (
-        <div>
-            <PageTitle pageTitle={"Welcome"} description={"Welcome to fitness app"}/>
-            <a href="/activity">Enter</a>
-        </div>
+      <div>
+        <PageTitle pageTitle={"Welcome"} description={"Welcome to fitness app"} />
+        <nav className='home-nav'>
+          <a href="/activity">Log</a>
+          <a href="/exercises">View Exercises</a>
+          <a href="/exercises">View Workouts</a>
+        </nav>
+      </div>
     );
   }
 }
